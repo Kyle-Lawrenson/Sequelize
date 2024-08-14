f704d998eafda4573f8b0cb84d9a22f4ec26b5ab
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-08-14 16:40:06 +0100

    it works

---
+++ src/controllers/addBook.js
@@ -0,0 +1,27 @@
+const Book = require ("../db/models/bookmodel")
+
+const addBook = async (req, res) => {
+    try {
+        const result = await Book.create({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre
+        })
+    
+        console.log(result);
+    
+        res.status(201).json({
+            msg: "Book added to database successfully",
+            book: result
+        })
+    } catch (error) {
+        console.log(error.errors[0].message);
+        res.status(418).json({
+            msg: "Error see error code",
+            error: error.errors[0].messsage
+        })
+    }
+   
+}
+
+module.exports = addBook;
---
+++ src/db/connection.js
@@ -0,0 +1,13 @@
+const {Sequelize} = require("sequelize");
+
+const connection = new Sequelize(process.env.MYSQL_URI);
+
+try {
+    connection.authenticate();
+    console.log("Connected to the database succesfully");
+   
+} catch (error) {
+    console.log(error)
+}
+ 
+module.exports = connection;
